e3fffa9b6b565f6b2558b4150174da2083e4372f
Can PERK <bayms8416@gmail.com>
2018-06-02 14:04:58 +0300

    oyun altyapısı oluşturuldu

---
+++ BilgeAdam.CardGame/BilgeAdam.CardGame/GameHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace BilgeAdam.CardGame
+{
+    public class GameHub : Hub
+    {
+        public void OpenCard(int id)
+        {
+            Clients.All.openCard(id);
+        }
+
+        public void Register(string name)
+        {
+            Clients.All.userRegistered(name);
+        }
+    }
+}
---
+++ BilgeAdam.CardGame/BilgeAdam.CardGame/Scripts/game.js
@@ -0,0 +1,44 @@
+﻿var game = {};
+game.hub = {};
+game.cards = [];
+$(document).ready(function () {
+    $("#btnConnect").click(function () {
+        $("#loginModal").modal();
+    });
+    $("#btnStart").click(function () {
+        game.cards = [];
+        var cardSet = ["10", "2", "4", "5", "8", "A", "J", "K", "Q", "R"];
+        game.hub.server.register("Can PERK");
+        while (game.cards.length < 20) {
+            var index = Math.floor(Math.random() * 10);
+            var card = cardSet[index];
+            var count = game.cards.filter(function (i) {
+                return i == card;
+            });
+            if (count.length < 2) {
+                game.cards.push(card);
+            }
+        }
+    });
+
+    $(".cardEmpty").click(function () {
+        var self = $(this);
+        self.removeClass("cardEmpty");
+        var index = self.attr("index");
+        var image = game.cards[index];
+        var path = "/images/" + image + ".png";
+        self.find(".cardContent").css("background-image", "url('" + path + "')");
+    });
+
+    game.hub = $.connection.gameHub;
+    game.hub.client.openCard = function (id) {
+        alert(id);
+    };
+    game.hub.client.userRegistered = function (name) {
+        alert(name + " oyuna katıldı");
+    }
+    $.connection.hub.start().done(function () {
+        $("#btnStart").removeAttr("disabled");
+        $("#loginModal").modal("close");
+    });
+});
