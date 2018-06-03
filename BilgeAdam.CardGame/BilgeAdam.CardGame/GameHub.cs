ac40f22389692b9eec5fdfc6a8e58da5639c9506
Can PERK <bayms8416@gmail.com>
2018-06-03 14:07:26 +0300

    oyun domaini oluşturuldu

--- BilgeAdam.CardGame/BilgeAdam.CardGame/GameHub.cs
+++ BilgeAdam.CardGame/BilgeAdam.CardGame/GameHub.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BilgeAdam.CardGame
 {
     public class GameHub : Hub
     {
+        static List<Game> games = new List<Game>();
+        static List<Gamer> gamers = new List<Gamer>();
         public void OpenCard(int id)
         {
             Clients.All.openCard(id);
@@ -11,7 +17,88 @@ namespace BilgeAdam.CardGame
 
         public void Register(string name)
         {
-            Clients.All.userRegistered(name);
+            var gamer = new Gamer { Id = Context.ConnectionId, Name = name, Status = Status.Guest };
+            Clients.All.userRegistered(gamer.Id, gamer.Name);
+            gamers.Add(gamer);
         }
+
+        public void StartGame()
+        {
+            var game = new Game(Context.ConnectionId);
+            games.Add(game);
+            var gamer = gamers.FirstOrDefault(i => i.Id == Context.ConnectionId);
+            if (gamer != null)
+            {
+                gamer.Status = Status.Waiting;
+            }
+            Clients.All.userStartedGame(Context.ConnectionId);
+        }
+
+        public void GetUsers()
+        {
+            Clients.Caller.loadUsers(gamers);
+        }
+
+        public void JoinGame(string ownerId)
+        {
+            var game = games.FirstOrDefault(i => i.User1 == ownerId);
+            if (game != null)
+            {
+                game.IsAvaliable = false;
+            }
+            var user1 = gamers.FirstOrDefault(i => i.Id == ownerId);
+            if (user1 != null)
+            {
+                user1.Status = Status.Busy;
+            }
+            var user2 = gamers.FirstOrDefault(i => i.Id == Context.ConnectionId);
+            if (user2 != null)
+            {
+                user2.Status = Status.Busy;
+            }
+            Clients.All.usersStartedToGame(user1.Id, user2.Id);
+        }
+
+        public async override Task OnDisconnected(bool stopCalled)
+        {
+            var gamer = gamers.FirstOrDefault(i => i.Id == Context.ConnectionId);
+            if (gamer != null)
+            {
+                gamers.Remove(gamer);
+            }
+            var userGames = games.Where(i => i.User1 == gamer.Id || i.User2 == gamer.Id).ToList();
+            userGames.ForEach(i => games.Remove(i));
+            Clients.Others.removeUser(gamer.Id);
+            await Task.CompletedTask;
+        }
+    }
+    class Game
+    {
+        public Game(string ownerId)
+        {
+            Id = Guid.NewGuid().ToString();
+            StartDate = DateTime.Now;
+            User1 = ownerId;
+            IsAvaliable = true;
+        }
+        public string Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public string User1 { get; set; }
+        public string User2 { get; set; }
+        public bool IsAvaliable { get; set; }
+    }
+
+    class Gamer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Status Status { get; set; }
+    }
+
+    enum Status
+    {
+        Waiting = 0,
+        Busy = 1,
+        Guest = 2
     }
 }
--- BilgeAdam.CardGame/BilgeAdam.CardGame/Scripts/game.js
+++ BilgeAdam.CardGame/BilgeAdam.CardGame/Scripts/game.js
@@ -1,14 +1,17 @@
 ﻿var game = {};
 game.hub = {};
 game.cards = [];
+
+var userStatus = {};
+userStatus.waiting = 0;
+userStatus.busy = 1;
+userStatus.guest = 2;
+
 $(document).ready(function () {
-    $("#btnConnect").click(function () {
-        $("#loginModal").modal();
-    });
     $("#btnStart").click(function () {
         game.cards = [];
         var cardSet = ["10", "2", "4", "5", "8", "A", "J", "K", "Q", "R"];
-        game.hub.server.register("Can PERK");
+        
         while (game.cards.length < 20) {
             var index = Math.floor(Math.random() * 10);
             var card = cardSet[index];
@@ -20,7 +23,6 @@ $(document).ready(function () {
             }
         }
     });
-
     $(".cardEmpty").click(function () {
         var self = $(this);
         self.removeClass("cardEmpty");
@@ -29,16 +31,106 @@ $(document).ready(function () {
         var path = "/images/" + image + ".png";
         self.find(".cardContent").css("background-image", "url('" + path + "')");
     });
-
     game.hub = $.connection.gameHub;
     game.hub.client.openCard = function (id) {
         alert(id);
     };
-    game.hub.client.userRegistered = function (name) {
-        alert(name + " oyuna katıldı");
+    game.hub.client.userRegistered = function (id, name) {
+        var newGamer = new gamer(id, name, userStatus.guest);
+        if (id == game.hub.connection.id) {
+            newGamer.isSelf = true;
+        }
+        users.gamers.push(newGamer);
+    }
+    game.hub.client.userStartedGame = function (clientId) {
+        var user = users.gamers.filter(function (u) {
+            return u.id == clientId;
+        });
+        if (user && user.length == 1) {
+            user[0].status = userStatus.waiting;
+        }
+    }
+    game.hub.client.loadUsers = function (gamers) {
+        for (var g in gamers) {
+            var ng = new gamer(gamers[g].Id, gamers[g].Name, gamers[g].Status);
+            users.gamers.push(ng);
+        }
+    }
+    game.hub.client.removeUser = function (id) {
+        var user = users.gamers.filter(function (u) {
+            return u.id == id;
+        });
+
+        if (user && user.length == 1) {
+            var index = users.gamers.indexOf(user[0]);
+            users.gamers.splice(index, 1);
+        }
+    }
+    game.hub.client.usersStartedToGame = function (user1, user2) {
+        users.gamers.filter(function (i) {
+            var index = users.gamers.indexOf(i);
+            if (i.id == user1) {
+                users.gamers[index].status = userStatus.busy;
+            }
+            else if (i.id == user2)
+            {
+                users.gamers[index].status = userStatus.busy;
+            }
+        });
+        
     }
     $.connection.hub.start().done(function () {
-        $("#btnStart").removeAttr("disabled");
-        $("#loginModal").modal("close");
+        game.hub.server.getUsers();
     });
 });
+var connector = new Vue({
+    el: "#connectArea",
+    data: {
+        name: "",
+        connected : false
+    },
+    methods: {
+        openDialog: function () {
+            $("#loginModal").modal("toggle");
+        },
+        connect: function () {
+            game.hub.server.register(this.name);
+            $("#loginModal").modal("toggle");
+            connector.connected = true;
+        }
+    }
+});
+var users = new Vue({
+    el: "#userList",
+    data: {
+       gamers : []
+    },
+    methods: {
+        startGame: function () {
+            game.hub.server.startGame();
+        },
+        join: function (g) {
+            if (game.hub.connection.id == g.id) {
+                return;
+            }
+            if (g.status != 0) {
+                toastr.warning("Yalnızca oyun oluşturan kullanıcıları seçebilirsiniz");
+                return;
+            }
+            game.hub.server.joinGame(g.id);
+        }
+    }
+});
+var gameArea = new Vue({
+    el: "",
+    data: {
+
+    }
+});
+var gamer = function (id, name, status) {
+    this.id = id;
+    this.name = name;
+    this.status = status;
+    this.isSelf = false;
+}
+
